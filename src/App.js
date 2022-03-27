55559cd5bcbdb9bbff8674f69f19c3ae3e844956
unknown <marioban3@gmail.com>
2022-03-27 16:02:07 +0200

    added global styling using createGlobalStyle from styled-components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,66 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap');
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body{
+  font-family: 'Poppins', sans-serif;
+}
+
+ul{
+  list-style-type:none;
+}
+
+a{
+  cursor:pointer;
+  text-decoration:none;
+  color:currentColor;
+}
+`;
+
+class App extends React.Component {
+  state = {};
+  render() {
+    return (
       <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link exact to="/">
-                  Coins
-                </Link>
-              </li>
-              <li>
-                <Link exact to="/portfolio">
-                  Portfolio
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route path="/portfolio">
-              <h1>Portfolio</h1>
-            </Route>
-            <Route path="/">
-              <h1>Coins</h1>
-            </Route>
-          </Switch>
+        <GlobalStyle />
+        <div className="App">
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link exact to="/">
+                    Coins
+                  </Link>
+                </li>
+                <li>
+                  <Link exact to="/portfolio">
+                    Portfolio
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            <Switch>
+              <Route path="/portfolio">
+                <h1>Portfolio</h1>
+              </Route>
+              <Route path="/">
+                <h1>Coins</h1>
+              </Route>
+            </Switch>
+          </div>
         </div>
       </Router>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
